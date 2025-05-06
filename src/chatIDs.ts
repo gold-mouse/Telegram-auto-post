39d610a8b86714b1fb2c6ab07fe4945981a9b492
Gold Mouse <ms0606.papa@gmail.com>
2025-05-07 01:42:19 +0900

    [update] code optimize

--- src/chatIDs.ts
+++ src/chatIDs.ts
@@ -1,1 +1,1 @@
-export const IDs: string[] = ['web_scraping_with_python', 'web_dev_support', 'webscrapinghub'];
+export const IDs: string[] = ['web_scraping_with_python', 'web_dev_support', 'selenium_java_clear_doubs', 'jobselenium', 'Selenium_Automation_Group', 'seleniumtestingpracticegroup'];
--- src/index.ts
+++ src/index.ts
@@ -4,142 +4,52 @@ import { CONFIG, SESSIONS_DIR } from './config';
 
 import fs from 'fs'
 
-try {
-    fs.mkdirSync(SESSIONS_DIR, { recursive: true });
-} catch (err) {
-    console.error('Failed to create sessions directory:', err);
-}
+fs.mkdirSync(SESSIONS_DIR, { recursive: true });
 
 var sentMessages = []; // Array to keep track of sent messages  
-var banedGroup: string[] = []; // Array to keep track of sent messages
+var banedGroup = []; // Array to keep track of sent messages
 
 const runBot = async () => {
-    console.log('------------------- Bot Started -------------------');
 
     const client = await authenticate(CONFIG['api_id'], CONFIG['api_hash']);
 
-    var expireTime = 0
+    const sleep = (min: number) => new Promise((resolve) => setTimeout(resolve, min * 1000 * 60));
 
-    // Function to format the date to YYYY-MM-DD HH:mm:ss  
-    const formatDateToString = (date: Date) => {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based  
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    }
-
-    const formatDurationFromMilliseconds = (milliseconds: number) => {
-        // Constants for time conversions  
-        const secondsInHour = 3600; // 60 minutes * 60 seconds  
-        const secondsInMinute = 60;
-
-        // Calculate total seconds from milliseconds  
-        const totalSeconds = Math.floor(milliseconds / 1000);
-
-        // Calculate hours, minutes, and seconds  
-        const hours = Math.floor(totalSeconds / secondsInHour);
-        const minutes = Math.floor((totalSeconds % secondsInHour) / secondsInMinute);
-        const seconds = totalSeconds % secondsInMinute;
-
-        // Construct the formatted string  
-        return `${hours} hour${hours !== 1 ? 's' : ''} ${minutes} min ${seconds} s`;
-    }
-
-    // The functin to generate timeouts between 1 and 1 hour 30 minutes
-    const generateRandomDuration = () => {
-        // 1 hour in milliseconds  
-        const oneHour = 60 * 60 * 1000; // 3,600,000 milliseconds  
-        // 1 hour 30 minutes in milliseconds  
-        const oneHourThirtyMinutes = (1 * 60 + 30) * 60 * 1000; // 5,400,000 milliseconds  
-
-        // Generate a random number between oneHour and oneHourThirtyMinutes  
-        const randomDuration = Math.floor(Math.random() * (oneHourThirtyMinutes - oneHour + 1)) + oneHour;
-
-        return Number(randomDuration); // Returns the duration in milliseconds  
-    }
-
-    // The function to set the expiration time
-    const setExpirationTime = async () => {
-        let duration = await generateRandomDuration();
-        expireTime = new Date().getTime() + duration;
-        console.log('Next posting will start after ', formatDurationFromMilliseconds(duration));
-        console.log('Next Posting Time: ', formatDateToString(new Date(new Date().getTime() + duration)) + '\n');
-        return duration
-    }
-
-    const postingStart = async () => {
-        if (expireTime < new Date().getTime()) {
-            console.log('******************** Posting started ********************')
-            await setExpirationTime()
-            await postToChannel()
-        }
-    }
-
-    // Function to delete sent messages  
     const deletePreviousMessages = async () => {
-        if (sentMessages.length == 0) {
-            console.log('~~~~~~~~~~~~~~~~~~~~ There is no message to delete ~~~~~~~~~~~~~~~~~~~~')
+        if (sentMessages.length == 0)
             return true
-        }
-        // Delete the messages in the sentMessages array one by one
 
-        try {
-            for (const { chatId, messageId } of sentMessages) {
-                await client.deleteMessages(chatId, [messageId], { revoke: false });
-                console.log('Message deleted successfully:', messageId);
-            }
-            // Clear the sentMessages array after deletion  
-            sentMessages.length = 0;
-            return true
-        } catch (error) {
-            console.error('Error deleting messages:', error);
-        }
+        for (let { chatId, messageId } of sentMessages)
+            await client.deleteMessages(chatId, [messageId], { revoke: false });
+
+        sentMessages = [];
+        return true
     };
 
-    // The function to post on some channel
-    const postToChannel = async () => {
+    while (true) {
+        try {
+            const deleted = await deletePreviousMessages();
+            if (!deleted) {
+                sentMessages = []
+                continue
+            }
+            for (let i = 0; i < IDs.length; i++) {
+                if (banedGroup.includes(IDs[i])) continue
 
-        // Delete previous messages before sending new ones  
-        const deleted = await deletePreviousMessages();
-        if (!deleted) return;
-        console.log('<<<<<<---------------------------->>>>>')
+                let before_banned = banedGroup.length
+                client.sendMessage(IDs[i], { message: "YOUR MESSAGE HERE." })
+                    .then(result => sentMessages.push({ chatId: IDs[i], messageId: result.id }))
+                    .catch(_ => banedGroup.push(IDs[i]))
+                    .finally(() => console.log(`${before_banned == banedGroup.length ? 'Sent' : 'Failed to send'} message to ${IDs[i]}`));
 
-        for (let i = 0; i < IDs.length; i++) {
-            if (banedGroup.includes(IDs[i])) continue
-            setTimeout(async () => {
-                try {
-                    const result = await client.sendMessage(IDs[i], {
-                        message: "YOUR MESSAGE HERE."
-                    });
-                    sentMessages.push({ chatId: IDs[i], messageId: result.id });
-                    console.log('^_^ . ^_^ . ^_^ . ^_^ Posted Successfully! ^_^ . ^_^ . ^_^ . ^_^');
-                } catch (error) {
-                    banedGroup.push(IDs[i])
-                    console.info(IDs[i])
-                    console.error('Error sending message:', error);
-                }
-            }, 1000 * 60); // post every 60 seconds
+                await sleep(5); // Sleep for 5 minutes
+            }
+        } catch (_) {
+            console.error('Failed to delete messages');
         }
+        sleep(60 * 3) // Sleep for 3 hours
     }
-
-    const serverStart = async () => {
-        console.log('******************* Server is running now ****************')
-        await postToChannel()
-        console.log('===========      First Message Is Sent Successfully      =========')
-        await setExpirationTime()
-        setInterval(() => {
-            postingStart()
-        }, 60 * 1000 * 5) // check if it's time to post every 10 minutes
-    }
-
-    serverStart()
 
 };
 
-(async () => {
-    await runBot().catch(console.error);
-})()
+runBot()
